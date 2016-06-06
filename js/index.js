06e68dfdb7ca4f69af07f73092208cb7d8f25658
Scott Pierce <scott.pierce@centro.net>
2016-06-06 11:37:33 -0500

    Initial export from http://codepen.io/ddrscott/pen/pbJZoW

---
+++ js/index.js
@@ -0,0 +1,143 @@
+'use strict';
+
+var Cell = function Cell(row, col, max, seed) {
+  this.row = row;
+  this.col = col;
+  this.max = max;
+
+  this.psuedoRandomInt = function (max, inc) {
+    var digits = Math.sin(inc) * 1000 * max;
+    return parseInt(Math.abs(digits)) % max + 1;
+  };
+
+  this.num = this.psuedoRandomInt(max, col * row + col + row + seed);
+};
+
+var App = {
+  rows: $('input[name="rows"]'),
+  cols: $('input[name="cols"]'),
+  seed: $('input[name="seed"]'),
+  btnResize: $('.resize'),
+  btnSolve: $('.solve'),
+  btnRandom: $('.random'),
+  puzzle: [],
+
+  init: function init(opts) {
+    this.max = opts.max;
+    this.rows.val(opts.rows);
+    this.cols.val(opts.cols);
+    this.btnResize.on('click', this.handleGenerate.bind(this));
+    this.btnSolve.on('click', this.handleSolve.bind(this));
+    this.btnRandom.on('click', this.handleRandom.bind(this));
+    this.seed.on('change', this.handleSeedChange.bind(this));
+
+    // always start with something
+    this.handleRandom();
+  },
+  handleSeedChange: function handleSeedChange(e) {
+    $('.puzzle').attr('data-seed', this.seed.val());
+  },
+  handleGenerate: function handleGenerate(e) {
+    this.puzzle = this.generate(this.rows.val(), this.cols.val());
+    this.update();
+  },
+  handleRandom: function handleRandom(e) {
+    this.seed.val(parseInt(Math.random() * 1000));
+    this.seed.change();
+    this.handleGenerate(e);
+  },
+  handleSolve: function handleSolve(e) {
+    var _this = this;
+
+    var puzzle = this.puzzle;
+    puzzle.forEach(function (row) {
+      row.forEach(function (cell) {
+        _this.markSums(puzzle, cell);
+      });
+    });
+  },
+
+  /**
+   * Adds 'match2-4' to any cells which sum to the origin cell.
+   */
+  markSums: function markSums(puzzle, origin) {
+    var _this2 = this;
+
+    var dirs = [[1, 0], // right
+    [-1, 0], // left
+    [0, 1], // down
+    [0, -1], // up
+    [1, 1], // right/down
+    [1, -1], // right/up
+    [-1, 1], // left/down
+    [-1, -1]];
+    // left/up
+    dirs.forEach(function (dir) {
+      origin.matches = _this2.sumDirection(puzzle, origin, dir);
+      if (origin.matches) {
+        var matchCls = 'match' + origin.matches.length;
+        origin.matches.forEach(function (m) {
+          return m.elm.addClass(matchCls);
+        });
+        origin.elm.addClass(matchCls);
+      }
+    });
+  },
+
+  /**
+   * Sums up cells in direction `dir`. Until it's equal or too many.
+   * There must be at least 2 cells.
+   * @return an array of matching cells or false
+   */
+  sumDirection: function sumDirection(puzzle, cell, dir) {
+    var cells = [],
+        sum = 0;
+    do {
+      var nextRow = puzzle[dir[0] * (cells.length + 1) + cell.row];
+      if (nextRow == undefined) {
+        return false;
+      }
+      var nextCell = nextRow[dir[1] * (cells.length + 1) + cell.col];
+      if (nextCell == undefined) {
+        return false;
+      }
+      cells.push(nextCell);
+      sum += nextCell.num;
+    } while (sum < cell.num);
+    if (sum == cell.num && cells.length > 1) {
+      return cells;
+    }
+    return false;
+  },
+  update: function update() {
+    this.render(this.puzzle);
+  },
+  render: function render(puzzle) {
+    var table = $('<table />');
+    puzzle.forEach(function (row) {
+      var tr = $('<tr/>');
+      row.forEach(function (col) {
+        col.elm = $('<td/>').text(col.num);
+        tr.append(col.elm);
+      });
+      table.append(tr);
+    });
+    $('.puzzle').html(table);
+  },
+  generate: function generate(numRows, numCols) {
+    var rows = [];
+    for (var r = 0; r < numRows; r++) {
+      var cols = [];
+      for (var c = 0; c < numCols; c++) {
+        cols.push(new Cell(r, c, this.max, this.seed.val()));
+      }
+      rows.push(cols);
+    }
+    return rows;
+  }
+};
+App.init({
+  rows: 10,
+  cols: 15,
+  max: 15
+});
