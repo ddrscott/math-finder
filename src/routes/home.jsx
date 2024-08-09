a1df6fc2b893e6cd8a99b32d108ddf091eb335e0
Scott Pierce <ddrscott@gmail.com>
2024-08-09 16:25:04 -0500

    Add dist and update github actions

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -7,31 +7,31 @@ export default function Root() {
             <div className="hero bg-base-100 min-h-[80vh]">
                 <div className="hero-content text-center flex-col px-8">
                     <h1 className="text-4xl font-bold">Math Finder</h1>
-                    <p className="text-lg">Learn math with your kids, together.</p>
+
+                    "Bond" with your kids by finding math together! 
 
                     <img src={demo} alt="demo" className="w-full" />
 
-                    <a className="btn btn-secondary m-8" href="play" >Start Playing</a>
+                    <a className="btn btn-secondary m-8" href="play" >Start Searching</a>
                 </div>
             </div>
             <div className="hero bg-secondary text-secondary-content py-[15%] shadow">
                 <div className="hero-content text-start flex-col px-8">
-                    <h2 className="text-3xl font-bold my-8">What is Math Finder?</h2>
-
-                    <p>Math Finder is an interactive and printable math app designed to help kids practice the essential math concept of number bonds. With our engaging game, you can bond with your child over math and watch them grow in confidence and fluency.</p>
+                    <h2 className="text-3xl font-bold my-8">What's Math Finder?</h2>
+                    <p>Math Finder is a fun app that helps kids learn addition bonds in an interactive or printable way. Just like solving a word search, we search for numbers that add up to make the equation true!</p>
                 </div>
             </div>
             <div className="hero bg-base-100 text-base-100-content py-[15%] shadow">
                 <div className="hero-content text-center flex-col px-8">
-                    <h2 className="text-3xl font-bold my-8">How Does Math Finder Work?</h2>
+                    <h2 className="text-3xl font-bold my-8">How Does it Work?</h2>
 
-                    <p>Math Finder uses interactive exercises to help kids learn number bonds with addition and subtraction. Our app provides a fun and engaging way for kids to practice math concepts in a safe and supportive environment.</p>
+                    <p>Math Finder uses a grid where we find numbers that add up make the addition bond! For instance, we search for <code class="code">4 + 3 = 7</code>, or any other addition bond. When we find it, circle it. Try to find them all!</p>
 
                     <img src={example} alt="example 4 + 3 = 7" />
                 </div>
             </div>
             <div className="hero bg-secondary text-secondary-content py-[15%] shadow">
-                <p className="text-center px-8">Made with ❤️ by <a href="https://ddrscott.github.com/" className="link" target="_blank">Scott</a></p>
+                <p className="text-center px-8">Made with ❤️ by <a href="https://ddrscott.github.com/" className="link" target="_blank">Scott</a>, for my kids and yours!</p>
             </div>
         </div>
     );
